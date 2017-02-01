44925495515ecb2e11d9ad4710c083ab00c95c4a
Patrick Tan <patrickmtan@lewisu.edu>
2017-02-01 04:16:45 +0000

    added first.rb

---
+++ first.rb
@@ -0,0 +1,12 @@
+print "Enter your name: "
+name = gets.chomp
+puts "Hello " + name
+# could also do puts "Hello" << name
+
+# for conversion 
+print "How many cups: "
+cups = gets.chomp
+# to put in an integer
+cups= cups.to_i
+ounces = cups * 8
+puts "That is #{ounces} ounces"
